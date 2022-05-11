13963d577e929da191544463a98b592aecc02c07
FrejaPayne <94685221+FrejaPayne@users.noreply.github.com>
2022-05-11 13:41:22 +0200

    Add files via upload

---
+++ js/slutprojekt.js
@@ -0,0 +1,24 @@
+'use strict';
+var slideIndex = 0;
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
+showSlides();
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
